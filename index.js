24e9f20e95726a6dc360fd3a6ec27f256bfda953
vschan2 <chanveisiang@hotmail.my>
2018-10-03 17:20:30 +0800

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.content.match(/[a-z]/gi).join("").toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
